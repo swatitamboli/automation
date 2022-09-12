5d75cb793709e3f04423cdca75df8dbe27545df5
swatitamboli <swatitamboli27@gmail.com>
2022-09-12 19:17:10 +0530

    add the modified TimePage TimePageTest HomePage constans class

--- src/main/java/pages/TimePage.java
+++ src/main/java/pages/TimePage.java
@@ -1,5 +1,22 @@
 package pages;
 
-public class TimePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class TimePage {
+	@FindBy(xpath = "//input[@placeholder='Type for hints...']")
+	WebElement empName;
+WebDriver driver;
+public TimePage(WebDriver driver)
+{
+	this.driver = driver;
+	PageFactory.initElements(driver, this);
+}
+public String getEmployeeName() 
+{
+	empName.sendKeys("swati");
+	return empName.getText();
+}
 }
--- src/test/java/tests/TimePageTest.java
+++ src/test/java/tests/TimePageTest.java
@@ -1,5 +1,18 @@
 package tests;
 
-public class TimePageTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import basetest.BaseTest;
+import constants.Constants;
+import pages.TimePage;
+
+public class TimePageTest extends BaseTest {
+@Test
+public void verifyEmployeeName()
+{
+	TimePage timepage=homePage.navigateToTimePage();
+	String actual=timepage.getEmployeeName();
+	Assert.assertEquals(actual, Constants.TIME_PAGE_EMPOLYEENAME);
+}
 }
